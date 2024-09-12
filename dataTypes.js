7599f948bb7e4695680e0e1463407decb91bf70f
MD JAHIDUL ISLAM JIHAD <mdj847621@gmail.com>
2024-09-12 06:39:25 +0600

    complete homework section

---
+++ dataTypes.js
@@ -0,0 +1,26 @@
+/*
+                          
+                                                ! HOME WORK
+
+        *1. Create a function that takes two numbers and returns their Multiplication.
+
+        *2. Write a function that prints "Good Morning!" if it's morning (use a parameter), otherwise prints "Good Evening!".
+
+        *3. Create a simple calculator that asks the user to input two numbers. Then, use arithmetic operators to calculate and display the following: Sum ,Difference, Product,Quotient,Remainder.
+
+        *4. Write a program that prompts the user to input their age and checks if the user is eligible to vote. Use comparison operators to compare the age to 18.
+
+        *5.  If the score is greater than or equal to 50, display "You passed!", otherwise display "You failed!" Use the ternary operator for this
+
+
+                                                ! extra homework
+                                                
+                                                * 1. function ki?
+                                                * 2. scope ki?
+                                                * 3. js expressions ki?
+                                                * 4. js operators ki?
+*/ 
+
+                                        
+
+                                        
